c2a335ff910eddffe1b37b8ac8a0c6d4f4959c22
sikle00 <92247756+sikle00@users.noreply.github.com>
2024-02-08 14:37:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function color() {
+    document.getElementById("intro").style.backgroundColor = "blue";
+    document.getElementById("gaming").style.backgroundColor = "blue";
+    document.getElementById("skiskyting").style.backgroundColor = "blue";
+    document.getElementById("jakt").style.backgroundColor = "blue";
+    document.getElementById("fiske").style.backgroundColor = "blue";
+    document.getElementById("intro").style.color = "white";
+    document.getElementById("gaming").style.color = "white";
+    document.getElementById("skiskyting").style.color = "white";
+    document.getElementById("jakt").style.color = "white";
+    document.getElementById("fiske").style.color = "white";
+}
+function colorR() {
+    document.getElementById("intro").style.backgroundColor = "red";
+    document.getElementById("gaming").style.backgroundColor = "red";
+    document.getElementById("skiskyting").style.backgroundColor = "red";
+    document.getElementById("jakt").style.backgroundColor = "red";
+    document.getElementById("fiske").style.backgroundColor = "red";
+    document.getElementById("intro").style.color = "black";
+    document.getElementById("gaming").style.color = "black";
+    document.getElementById("skiskyting").style.color = "black";
+    document.getElementById("jakt").style.color = "black";
+    document.getElementById("fiske").style.color = "black";
+}
+function changePicture() {
+    document.getElementById("audun").style.opacity = "0";
+    document.getElementById("audun2").style.opacity = "100";
+    document.getElementById("audun3").style.opacity = "0";
+}
+function changePicture1() {
+    document.getElementById("audun2").style.opacity = "0";
+    document.getElementById("audun").style.opacity = "100";
+    document.getElementById("audun3").style.opacity = "0";
+}
+function changePicture2() {
+    document.getElementById("audun2").style.opacity = "0";
+    document.getElementById("audun").style.opacity = "0";
+    document.getElementById("audun3").style.opacity = "100";
+}
